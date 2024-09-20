82a3c33ac0adafbf8529413f4e3560486b6cd8bc
Yeder <ypimentel.tapia@gmail.com>
2024-09-20 16:49:15 +0200

    create twitter card

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,19 @@
+export function App () {
+
+    return(
+       <article>
+        <header>
+            <img src="../src/assets/twitter.png" alt="" />
+            <div>
+                <strong>Yeder Pimentel</strong>
+                <span>@yederpt</span>
+            </div>
+        </header>
+        <aside>
+            <button>
+                seguir
+            </button>
+        </aside>
+       </article>
+    )
+}
